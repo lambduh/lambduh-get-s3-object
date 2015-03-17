ddb54d34c0669d00e2c0a7e704c119658a350b0d
Russell Matney <russell.matney@gmail.com>
2015-03-17 01:38:59 -0400

    validation tests, implementation

--- test/put-s3-object.spec.js
+++ test/put-s3-object.spec.js
@@ -14,4 +14,81 @@ describe('getS3Object', function() {
   it('should return a function that returns a promise', function() {
     expect(get()().then).to.exist;
   });
+
+ describe('validation', function() {
+    it('should throw an error on null options input', function(done) {
+      get()().then(function() {
+        done(new Error('Expected function to throw error'));
+      }, function(err) {
+        if (err) {
+          done();
+        } else {
+          done(new Error('Expected err object to exist'));
+        }
+      })
+    });
+
+    it('should require srcBucket param', function(done) {
+      get()({}).then(function() {
+        done(new Error('Expected function to throw error'));
+      }, function(err) {
+        if (err) {
+          done();
+        } else {
+          done(new Error('Expected err object to exist'));
+        }
+      })
+    });
+
+    it('should require srcKey param', function(done) {
+      var options = {
+        srcBucket: "my-lil-red-bucket"
+      }
+      get()(options).then(function() {
+        done(new Error('Expected function to throw error'));
+      }, function(err) {
+        if (err) {
+          done();
+        } else {
+          done(new Error('Expected err object to exist'));
+        }
+      })
+    });
+
+    it('should require downloadFilepath param', function(done) {
+      var options = {
+        srcBucket: "my-lil-red-bucket",
+        srcKey: "my-red-lil-key.png"
+      }
+      get()(options).then(function() {
+        done(new Error('Expected function to throw error'));
+      }, function(err) {
+        if (err) {
+          done();
+        } else {
+          done(new Error('Expected err object to exist'));
+        }
+      })
+    });
+  });
+
+  //TODO: implement properly - mock AWS.S3 .getObject()
+  it('should resolve the options object when required params are included', function(done) {
+    var options = {
+      srcBucket: "my-lil-red-bucket",
+      srcKey: "my-red-lil-key.png",
+      downloadFilepath: "/tmp/my-red-lil-key.png",
+      key: 'val'
+    }
+    get()(options).then(function(opts) {
+      if (opts == options) {
+        done();
+      } else {
+        done(new Error('Expected resolved options to match inputted options'));
+      }
+    }, function() {
+      done(new Error('Expected function to pass'));
+    })
+  });
+
 });
